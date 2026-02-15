16d4dee93df661030f72b40c1c0323cce212c34d
tmt115 <miketommy2006@gmail.com>
2026-02-14 21:14:23 -0500

    fix

--- XO.py
+++ XO.py
@@ -13,45 +13,46 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     batch_size=BATCH_SIZE,
     label_mode="int"
 )
-print(train_ds.class_names)
-# val_ds = tf.keras.utils.image_dataset_from_directory(
-#     "dataset/val",
-#     image_size=(IMG_SIZE, IMG_SIZE),
-#     color_mode="grayscale",
-#     batch_size=BATCH_SIZE,
-#     label_mode="int"
-# )
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "dataset/val",
+    image_size=(IMG_SIZE, IMG_SIZE),
+    color_mode="grayscale",
+    batch_size=BATCH_SIZE,
+    label_mode="int"
+)
 
 
-# model = tf.keras.models.Sequential([
-#     tf.keras.layers.Input(shape=(64, 64, 1)),
-#     tf.keras.layers.Rescaling(1./255),
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input(shape=(64, 64, 1)),
+    tf.keras.layers.Rescaling(1./255),
 
-#     tf.keras.layers.RandomRotation(0.2),
-#     tf.keras.layers.RandomTranslation(0.1, 0.1),
-#     tf.keras.layers.RandomZoom(0.1),
-#     tf.keras.layers.RandomContrast(0.3),
+    tf.keras.layers.RandomRotation(0.2),
+    tf.keras.layers.RandomTranslation(0.1, 0.1),
+    tf.keras.layers.RandomZoom(0.1),
+    tf.keras.layers.RandomContrast(0.3),
 
-#     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
 
-#     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-#     tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
 
-#     tf.keras.layers.Flatten(),
+    tf.keras.layers.Flatten(),
 
-#     tf.keras.layers.Dense(128, activation='relu'),
-#     tf.keras.layers.Dense(3, activation='softmax')
-# ])
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
 
-# model.compile(optimizer = tf.keras.optimizers.Adam(),
-#               loss = 'sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-# model.fit(
-#     train_ds,
-#     validation_data=val_ds,
-#     epochs=20
-# )
-# model.save("xoblank.keras")
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(
+    train_ds,
+    validation_data=val_ds,
+    epochs=20
+)
+model.save("xoblank.keras")
+
 
 
